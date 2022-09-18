86ba01b9b72773b63858acb4f803638cece2503c
AdedamolaOD <105971371+AdedamolaOD@users.noreply.github.com>
2022-09-18 01:37:41 +0100

    Update temperature-converter.py

--- temperature-converter.py
+++ temperature-converter.py
@@ -8,3 +8,10 @@ if x =='f':
     print(f"The temperature of {degree}°F is equivalent to {celsius_inF}°C")
 elif x == 'k':
     print(f"The temperature of {degree}°K is equivalent to {celsius_inK}°C ")
+    
+# Tried the code below but there was a semantic error 😳
+
+'''print(f"The temperature of {degree}°F is equivalent to {celsius_inF}°C") if x == "f" else print(f"The temperature of {degree}°K is equivalent to {celsius_inK}°C ")'''
+
+"""Thw error is that I could not add the condition if x == "k" because I couldn't add anything after the 'else' sstatement. So any other thing the user inputs
+other than f is going to run """
